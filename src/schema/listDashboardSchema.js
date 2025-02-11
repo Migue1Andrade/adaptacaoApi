b44d8607d83d0acd28fd25140d3033e3e7e5d2d6
Migue1Andrade <mfrancadeandrade@gmail.com>
2025-02-11 18:10:51 -0300

    refactor: enhance date transformation logic in dashboard schema validation

--- src/schema/listDashboardSchema.js
+++ src/schema/listDashboardSchema.js
@@ -1,15 +1,21 @@
 const yup = require('yup');
 
 const getDashboardSchema = yup.object().shape({
-	start_date:
-		yup.string()
-		.required('O campo start_date é obrigatório.')
-		.date(),
+    start_date:
+        yup.string()
+        .required('O campo start_date é obrigatório.')
+        .transform(function(value, originalValue) {
+            return originalValue ? new Date(originalValue.split('/').reverse().join('-')) : null;
+        })
+        .date(),
 
-	end_date:
-		yup.string()
-		.required('O campo end_date é obrigatório.')
-		.date(),
+    end_date:
+        yup.string()
+        .required('O campo end_date é obrigatório.')
+        .transform(function(value, originalValue) {
+            return originalValue ? new Date(originalValue.split('/').reverse().join('-')) : null;
+        })
+        .date(),
 }).noUnknown();
 
 module.exports = getDashboardSchema;
